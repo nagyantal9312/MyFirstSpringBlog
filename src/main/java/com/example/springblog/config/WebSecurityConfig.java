fc2cbcd96c59ef89ec3b636239ceb5850be26393
nagyantal9312 <nagyantal9312@gmail.com>
2021-05-04 22:47:29 +0200

    csrf

--- src/main/java/com/example/springblog/config/WebSecurityConfig.java
+++ src/main/java/com/example/springblog/config/WebSecurityConfig.java
@@ -43,6 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
+                .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/resources/**").permitAll()
                     .antMatchers("/css/**", "/images/**").permitAll()
